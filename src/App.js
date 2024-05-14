6348c874c01fa7a738453ceff6438cfd59445e52
Marketing3 <ariel.fuentesgarcia@gmail.com>
2024-05-14 17:16:36 -0600

    income expenses

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Balance from './components/Balance';
 import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
 import TransactionForm from './components/transactions/TransactionForm';
 import TransactionList from './components/transactions/TransactionList';
 import { GlobalProvider } from './context/GlobalState';
@@ -10,10 +11,10 @@ function App() {
     <div className='App'>
       <GlobalProvider>
         <Header />
+        <IncomeExpenses />
         <Balance />
         <TransactionForm />
         <TransactionList />
-        <h1>hello</h1>
       </GlobalProvider>
     </div>
   );
